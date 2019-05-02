78af44e90cf8b3c18e0650abbfbbc2aa0cedaa3f
Kingsley555 <kingsleyoha@hotmail.com>
2019-05-02 16:49:23 +0200

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://kingsley:ballack02@ds149676.mlab.com:49676/storybooks-dev',
-    googleClientID: '1064692350442-r0tpmfi8m4of4vfsvhacg3942rffbrrf.apps.googleusercontent.com',
-    googleClientSecret:'g40-TAQqHgYnYCj-b2InThkT'
-}
